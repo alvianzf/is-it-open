47c04d337316fa639427a86a6cc4320f83744b9d
Alvian Zachry Faturrahman <alvianzf@gmail.com>
2020-09-11 11:22:39 +0700

    Updates

---
+++ precache-manifest.b25771c112f8df0940a9d8421adacc79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd967a4b824da2c9565c63ca16ed37f",
+    "url": "/isitopen/index.html"
+  },
+  {
+    "revision": "25e37ad9b03d5e599a69",
+    "url": "/isitopen/static/css/main.6f262088.chunk.css"
+  },
+  {
+    "revision": "19b3716b249599965490",
+    "url": "/isitopen/static/js/2.69d8687e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/isitopen/static/js/2.69d8687e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e37ad9b03d5e599a69",
+    "url": "/isitopen/static/js/main.bc5fa23d.chunk.js"
+  },
+  {
+    "revision": "2aae0997bba3b3a98c2d",
+    "url": "/isitopen/static/js/runtime-main.43bea660.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/isitopen/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d67c62bbb6ed8675c7adcdfa33fe76ec.js"
+  "/isitopen/precache-manifest.b25771c112f8df0940a9d8421adacc79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/isitopen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
