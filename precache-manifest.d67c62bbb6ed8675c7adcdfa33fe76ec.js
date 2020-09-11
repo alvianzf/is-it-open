14e123ee1393741205193037b8a5374f963859a6
Alvian Zachry Faturrahman <alvianzf@gmail.com>
2020-09-11 11:14:35 +0700

    Updates

---
+++ precache-manifest.d67c62bbb6ed8675c7adcdfa33fe76ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5958f4bfdcb7a5cbb88b3f4fab1d93c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f45e4d31f03073e80e2d",
+    "url": "/static/css/main.6f262088.chunk.css"
+  },
+  {
+    "revision": "19b3716b249599965490",
+    "url": "/static/js/2.69d8687e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.69d8687e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45e4d31f03073e80e2d",
+    "url": "/static/js/main.0dd10027.chunk.js"
+  },
+  {
+    "revision": "9c64b83d44426cabad0b",
+    "url": "/static/js/runtime-main.ef87aa63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
